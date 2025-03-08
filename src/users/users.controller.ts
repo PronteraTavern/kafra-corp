23db67a8c5f897e8fd072f1f8c485ece88bbc831
lucasnDomingues <lucasn.domingues@gmail.com>
2025-03-08 12:33:00 -0300

    added routes for creating and deleting a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Request } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthenticatedRequest } from 'src/auth/interfaces/authenticated-request.interface';
 import { ValidatedUserDto } from './dtos/validated-user.dto';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Public } from 'src/public.decorator';
 
 @Controller('users')
 export class UserController {
@@ -10,7 +12,22 @@ export class UserController {
   @Get('profile')
   async getProfile(
     @Request() req: AuthenticatedRequest,
-  ): Promise<ValidatedUserDto | null> {
+  ): Promise<ValidatedUserDto> {
     return await this.userService.profile(req.user.id);
+  }
+
+  @Public()
+  @Post()
+  async createUser(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<ValidatedUserDto> {
+    return await this.userService.create(createUserDto);
+  }
+
+  @Delete()
+  async removeUser(
+    @Request() req: AuthenticatedRequest,
+  ): Promise<ValidatedUserDto> {
+    return await this.userService.remove(req.user.id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { ValidatedUserDto } from './dtos/validated-user.dto';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -15,12 +20,40 @@ export class UsersService {
     return this.userRespository.findOneBy({ email });
   }
 
-  async profile(id: string): Promise<ValidatedUserDto | null> {
+  findById(id: string): Promise<User | null> {
+    return this.userRespository.findOneBy({ id });
+  }
+
+  async profile(id: string): Promise<ValidatedUserDto> {
     const user = await this.userRespository.findOneBy({ id });
     if (user) {
       const { password_hash, ...result } = user;
       return result;
     }
-    return null;
+    throw new NotFoundException();
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<ValidatedUserDto> {
+    const user = await this.findByEmail(createUserDto.email);
+    if (user) {
+      //Lucas - This is probably wrong.
+      throw new BadRequestException();
+    }
+    const createdUser: User = await this.userRespository.save(createUserDto);
+    const { password_hash, ...result } = createdUser;
+
+    return result;
+  }
+
+  async remove(id: string): Promise<ValidatedUserDto> {
+    const user = await this.findById(id);
+    if (!user) {
+      //Lucas - This is probably wrong.
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    const deletedUser = await this.userRespository.remove(user);
+    const { password_hash, ...result } = deletedUser;
+
+    return result;
   }
 }
